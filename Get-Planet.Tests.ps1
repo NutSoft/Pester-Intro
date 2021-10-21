4677cea75d9fcaa4ea833460b182bdbe04a587a9
Des Finkenzeller <nutsoft@hotmail.com>
2021-10-21 12:15:08 +0100

    Adding more tests

--- Get-Planet.Tests.ps1
+++ Get-Planet.Tests.ps1
@@ -20,4 +20,21 @@ Describe 'Get-Planet' {
         $allPlanets = Get-Planet
         $allPlanets.Count | Should -Be 8
     }
+
+    It 'Earth is the 3rd planet in our solar system' {
+        $allPlanets = Get-Planet
+        $allPlanets[2].Name | Should -Be 'Earth'
+    }
+
+    It 'Pluto is no longer classified as a planet in our solar system' {
+        $allPlanets = Get-Planet
+        $plutos = $allPlanets | Where-Object Name -eq 'Pluto'
+        $plutos.Count | Should -Be 0
+    }
+
+    It 'Planets have this order: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune' {
+        $allPlanets = Get-Planet
+        $planetsInOrder = $allPlanets.Name -join ', ' 
+        $planetsInOrder | Should -Be 'Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune'
+    } 
 }
