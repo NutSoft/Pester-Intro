0e9871ef5d0ae2d8d6705281d08635b03e5b1c56
Des Finkenzeller <nutsoft@hotmail.com>
2021-10-21 11:49:01 +0100

    Revert to example after Pester update

--- Get-Planet.Tests.ps1
+++ Get-Planet.Tests.ps1
@@ -1,9 +1,20 @@
-# The directory of Get-Planet.Tests.ps1
-$script_dir = Split-Path -Parent $MyInvocation.MyCommand.Path
- 
-# Load the script to test into memory
-. "$script_dir\Get-Planet.ps1"
- 
+BeforeAll { 
+    function Get-Planet ([string]$Name = '*') {
+        $planets = @(
+            @{ Name = 'Mercury' }
+            @{ Name = 'Venus'   }
+            @{ Name = 'Earth'   }
+            @{ Name = 'Mars'    }
+            @{ Name = 'Jupiter' }
+            @{ Name = 'Saturn'  }
+            @{ Name = 'Uranus'  }
+            @{ Name = 'Neptune' }
+        ) | ForEach-Object { [PSCustomObject] $_ }
+
+        $planets | Where-Object { $_.Name -like $Name }
+    }
+}
+
 Describe 'Get-Planet' {
     It 'Given no parameters, it lists all 8 planets' {
         $allPlanets = Get-Planet
